74f59590a1afc2e887e1185430cee967a78d994a
riwz <nulj@ya.ru>
2020-04-30 20:10:11 +0400

    added task1

---
+++ src/com/company/task1.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class task1 {
+    public static Map<String, Date> createMap() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
+        Map<String, Date> map = new HashMap<>();
+        map.put("Сталлоне", dateFormat.parse("MAY 1 2012"));
+
+        return null;
+    }
+
+    public static void removeAllSummerPeople(Map<String, Date> map) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+Условие
+
+Создать словарь/мапу (Map<String, Date>) и занести в него десять записей по принципу: "фамилия" - "дата рождения".
+Удалить из словаря всех людей, родившихся летом.
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Программа не должна считывать значения с клавиатуры.
+3. Метод createMap() должен создавать и возвращать словарь HashMap с типом элементов String, Date состоящий из 10 записей.
+4. Метод removeAllSummerPeople() должен удалять из словаря всех людей, родившихся летом.
+*/
