96ffdc1a61772adca2c8649f249eece33c76d162
Alex <Brainiyc@yandex.ru>
2020-05-05 21:10:19 +0400

    task 1

--- src/com/company/task1.java
+++ src/com/company/task1.java
@@ -3,22 +3,29 @@ package com.company;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 public class task1 {
     public static Map<String, Date> createMap() throws ParseException {
         DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
-        Map<String, Date> map = new HashMap<>();
-        map.put("Сталлоне", dateFormat.parse("MAY 1 2012"));
-
+        Map<String, Date> map = new HashMap<String, Date>();
+        map.put("Сталлоне",dateFormat.parse("MAY 1 2012"));
+        map.put("Alex", dateFormat.parse("april 1 2012"));
+        map.put("John", dateFormat.parse("jun 17 2012"));
         return null;
     }
 
     public static void removeAllSummerPeople(Map<String, Date> map) {
+        for (Map.Entry<String, Date> e : map.entrySet()) {
+            String a = e.getKey();
+            Date b = e.getValue();
+            // int v;
+             //int i = v.getMoth();
+            String k = "";
+            // if (i == 2 || i == 3) map.remove(k);
 
+
+        }
 
     }
 
@@ -34,6 +41,7 @@ public class task1 {
 Удалить из словаря всех людей, родившихся летом.
 
 Требования:
+0. Распичатать Map.
 1. Программа не должна выводить текст на экран.
 2. Программа не должна считывать значения с клавиатуры.
 3. Метод createMap() должен создавать и возвращать словарь HashMap с типом элементов String, Date состоящий из 10 записей.
