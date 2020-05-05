e4430cab455d7ace6e849d41b93ed48fd7f44049
riwz <nulj@ya.ru>
2020-05-05 22:10:56 +0400

    added print matrix task with upgrade request

---
+++ src/com/company/PrintMatrix.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class PrintMatrix {
+    public static void main(String[] args) {
+        int[][] m = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+решение из серии "как вариант", но можно его улучшить, чтобы было более красиво,
+попробуй сделать без массивов, тем более двумерных и с помощью одного цикла.
++ обрати внимание на текущий warning, как идея предлогает улучшить этот код
+*/
