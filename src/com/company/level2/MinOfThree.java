eace158d000d7432486a399ac85325b9a994ccc1
Alex <Brainiyc@yandex.ru>
2020-06-05 20:42:39 +0400

    min three numbers

---
+++ src/com/company/level2/MinOfThree.java
@@ -0,0 +1,4 @@
+package com.company.level2;
+
+public class MinOfThree {
+}
--- src/com/company/level2/MinimumOfTwo.java
+++ src/com/company/level2/MinimumOfTwo.java
@@ -2,14 +2,21 @@ package com.company.level2;
 
 public class MinimumOfTwo {
 
-    public static int min(int a, int b) {
-        return Math.min(a, b);
+    public static int min(int a, int b, int c) {
+
+        if (a < b & b < c) {
+           return a;
+        } else if (b < a & b < c) {
+            return b;
+        } else {
+            return c;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(min(12, 33));
-        System.out.println(min(-20, 0));
-        System.out.println(min(-10, -20));
+        System.out.println(min(-12, 0, 45));
+        System.out.println(min(-20, 3, -22));
+        System.out.println(min(-10, -20, 44));
     }
 
 }
